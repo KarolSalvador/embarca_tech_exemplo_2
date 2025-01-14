7c79947084d95c26babe3245862d1c1561b2bfc5
Karol <karolsalvador91@gmail.com>
2025-01-14 06:22:27 -0300

    alteração dos botões de pull_down para pull_up e adicionado comparação no if

--- exemplo_2.c
+++ exemplo_2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#define LED_R_PIN 12 //Define o LED vermelho
-#define LED_G_PIN 13 //Define o LED verde
-#define LED_B_PIN 11 //Define o LED azul
+//Nesse ponto eu fiz as correções dos pinos
+#define LED_R_PIN 13 //Define o LED vermelho 
+#define LED_G_PIN 11 //Define o LED verde
+#define LED_B_PIN 12 //Define o LED azul
 #define BTN_A_PIN 5 //Define o botão A
 #define BTN_B_PIN 6 //Define o botao B
 
@@ -32,21 +33,22 @@ int main()
     //Inicializa os pinos dos botões como entrada
     gpio_init(BTN_A_PIN);
     gpio_set_dir(BTN_A_PIN, GPIO_IN);
-    gpio_pull_down(BTN_A_PIN);
-    
+    gpio_pull_up(BTN_A_PIN); //como boa pratica, alterei pra pull_up
+
     gpio_init(BTN_B_PIN);
     gpio_set_dir(BTN_B_PIN, GPIO_IN);
-    gpio_pull_down(BTN_B_PIN);
+    gpio_pull_up(BTN_B_PIN); //como boa pratica, alterei pra pull_up
 
     //Loop para verificar estado dos botões e controlar LEDs
     while (true) {
-        if(gpio_get(BTN_A_PIN) && gpio_get(BTN_B_PIN)) { //verifica se botão A e B estão pressionados
+    //Aqui eu alterei como o programa identifica se o botão foi acionado
+        if(gpio_get(BTN_A_PIN) == 0 && gpio_get(BTN_B_PIN) == 0 ) { //verifica se botão A e B estão pressionados
             set_leds(1, 1, 1); // Todos os LEDS acesos (branco)
 
-        } else if(gpio_get(BTN_A_PIN)) { // verifica se o apenas o botão A estiver pressionado
+        } else if(gpio_get(BTN_A_PIN) == 0 ) { // verifica se o apenas o botão A estiver pressionado
             set_leds(1, 0, 0); //LED vermelho acesso
 
-        } else if(gpio_get(BTN_B_PIN)) { //verifica se botão B está pressionado
+        } else if(gpio_get(BTN_B_PIN) == 0) { //verifica se botão B está pressionado
             set_leds(0, 1, 0); //LED verde aceso
 
         } else { // se nenhum botão estiver pressionado
@@ -54,8 +56,8 @@ int main()
         }
 
         sleep_ms(100);
-         
+
     }
     return 0;
-    
+
 }
